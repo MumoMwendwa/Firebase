087102a3d380e6ad75091e15d7f21829ab8c188e
MumoMwendwa <mwendwa05mumo@gmail.com>
2023-04-18 13:36:27 +0300

    first commit

---
+++ app/src/main/java/com/example/morningfirebasedbapp/User.kt
@@ -0,0 +1,19 @@
+package com.example.morningfirebasedbapp
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+class User {
+    var name:String = ""
+    var email:String = ""
+    var idNumber:String = ""
+    var id:String = ""
+
+
+    constructor(name: String ,email: String, idNumber: String, id:String){
+        this.name = name
+        this.email = email
+        this.idNumber = idNumber
+        this .id = id
+    }
+    constructor()
+}
